c4ec7a4b18475db51b5e6890ff411dbd1368dc0a
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2018-08-08 10:53:19 +0200

    Initial DB migration to obsolete init_db.

--- migrations/versions/6c1aaca139b1_.py
+++ migrations/versions/6c1aaca139b1_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '6c1aaca139b1'
-down_revision = None
+down_revision = '0a04bbfe404b'
 branch_labels = None
 depends_on = None
 
