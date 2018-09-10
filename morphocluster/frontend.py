3305f979b78d0ee57035c218313b07fb50557d9b
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2018-09-10 10:41:34 +0200

    Fix frontend

--- morphocluster/frontend.py
+++ morphocluster/frontend.py
@@ -1,8 +1,11 @@
 from flask.blueprints import Blueprint
 
 frontend = Blueprint("frontend", __name__,
-                static_folder="frontend_static", static_url_path="")
+                     static_folder="frontend_static", static_url_path="")
+
 
 @frontend.route("/")
-def index():
+@frontend.route("/p")
+@frontend.route("/p/<path:path>")
+def index(path=None):
     return frontend.send_static_file('index.html')
