668524e25e592c31056b0f0c951e1aa8fa8df5a8
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2018-09-25 10:57:39 +0200

    Configure PROJECT_EXPORT_DIR

--- morphocluster/config_default.py
+++ morphocluster/config_default.py
@@ -4,3 +4,6 @@ REDIS_URL = "redis://:@localhost:6379/0"
 # Database
 SQLALCHEMY_DATABASE_URI = "postgresql://morphocluster:morphocluster@localhost/morphocluster"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Project export directory
+PROJECT_EXPORT_DIR = "/tmp"
