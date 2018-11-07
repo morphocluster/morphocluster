1de3c7e093b52ae69c80bed82c3647b4f3d63e55
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2018-11-07 12:57:32 +0100

    config_default: statement_timeout=240s

--- morphocluster/config_default.py
+++ morphocluster/config_default.py
@@ -4,6 +4,11 @@ REDIS_URL = "redis://:@localhost:6379/0"
 # Database
 SQLALCHEMY_DATABASE_URI = "postgresql://morphocluster:morphocluster@localhost/morphocluster"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_OPTIONS = {
+    'connect_args': {
+        "options": "-c statement_timeout=240s"
+    }
+}
 
 # Project export directory
 PROJECT_EXPORT_DIR = "/tmp"
