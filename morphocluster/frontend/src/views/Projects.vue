7881167596702f3e77517ccc4461b3898c768e7d
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2019-03-18 12:35:51 +0100

    frontend.Projects: Save Modal.

--- morphocluster/frontend/src/views/Projects.vue
+++ morphocluster/frontend/src/views/Projects.vue
@@ -1,69 +1,153 @@
 <template>
-    <div id="projects">
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-            <router-link class="navbar-brand text-light" to="/">MorphoCluster</router-link>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active text-light">
-                    Projects
-                </li>
-            </ul>
-        </nav>
-        <div class="scrollable">
-            <div class="container">
-                <b-table id="projects_table" striped sort-by="name" :items="projects" :fields="fields" showEmpty>
-                    <template slot="table-colgroup">
-                        <col class="col-wide" />
-                        <col class="col-narrow" />
-                    </template>
-                    <template slot="name" slot-scope="data">
-                        <router-link :to="{name: 'project', params: {project_id: data.item.project_id}}">{{data.item.name}}</router-link>
-                    </template>
-                    <div slot="progress" slot-scope="data">
-                        <b-progress v-if="'progress' in data.item" :max="data.item.progress.leaves_n_nodes">
-                            <b-progress-bar variant="success" :value="data.item.progress.leaves_n_filled_nodes" v-b-tooltip.hover :title="`${data.item.progress.leaves_n_filled_nodes} / ${data.item.progress.leaves_n_nodes} filled up`" />
-                            <b-progress-bar variant="warning" :value="data.item.progress.leaves_n_approved_nodes - data.item.progress.leaves_n_filled_nodes" v-b-tooltip.hover :title="`${data.item.progress.leaves_n_approved_nodes} / ${data.item.progress.leaves_n_nodes} approved`" />
-                            <!-- <b-progress-bar variant="secondary" :value="data.item.progress.leaves_n_nodes - data.item.progress.leaves_n_approved_nodes" v-b-tooltip.hover :title="`${data.item.progress.leaves_n_nodes - data.item.progress.leaves_n_approved_nodes} / ${data.item.progress.leaves_n_nodes} untreated`" /> -->
-                        </b-progress>
-                    </div>
-                    <template slot="action" slot-scope="data">
-                        <b-button size="sm" variant="primary" class="mr-2" :to="{name: 'approve', params: {project_id: data.item.project_id}}">
-                            Approve
-                        </b-button>
-                        <b-button size="sm" variant="primary" class="mr-2" :to="{name: 'bisect', params: {project_id: data.item.project_id}}">Bisect</b-button>
-                        <b-button size="sm" variant="primary" class="mr-2" @click.prevent="showSaveModal(data.item)">Save</b-button>
-                    </template>
-                    <template slot="visible" slot-scope="data">
-                        {{data.visible ? "yes" : "no"}}
-                    </template>
-                    <template slot="empty">
-                        <div class="text-center">No projects available.</div>
-                    </template>
-                </b-table>
-                <p v-if="!projects">No projects available.</p>
-                <div style="margin: 0 auto; width: 0;">
-                    <b-button size="sm" variant="danger" class="mr-2" href="/labeling">
-                        Expert mode
-                    </b-button>
-                </div>
-            </div>
+  <div id="projects">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <router-link
+        class="navbar-brand text-light"
+        to="/"
+      >MorphoCluster</router-link>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active text-light">
+          Projects
+        </li>
+      </ul>
+    </nav>
+    <div class="scrollable">
+      <div class="container">
+        <b-table
+          id="projects_table"
+          striped
+          sort-by="name"
+          :items="projects"
+          :fields="fields"
+          showEmpty
+        >
+          <template slot="table-colgroup">
+            <col class="col-wide" />
+            <col class="col-narrow" />
+          </template>
+          <template
+            slot="name"
+            slot-scope="data"
+          >
+            <router-link :to="{name: 'project', params: {project_id: data.item.project_id}}">{{data.item.name}}</router-link>
+          </template>
+          <div
+            slot="progress"
+            slot-scope="data"
+          >
+            <b-progress
+              v-if="'progress' in data.item"
+              :max="data.item.progress.leaves_n_nodes"
+            >
+              <b-progress-bar
+                variant="success"
+                :value="data.item.progress.leaves_n_filled_nodes"
+                v-b-tooltip.hover
+                :title="`${data.item.progress.leaves_n_filled_nodes} / ${data.item.progress.leaves_n_nodes} filled up`"
+              />
+              <b-progress-bar
+                variant="warning"
+                :value="data.item.progress.leaves_n_approved_nodes - data.item.progress.leaves_n_filled_nodes"
+                v-b-tooltip.hover
+                :title="`${data.item.progress.leaves_n_approved_nodes} / ${data.item.progress.leaves_n_nodes} approved`"
+              />
+              <!-- <b-progress-bar variant="secondary" :value="data.item.progress.leaves_n_nodes - data.item.progress.leaves_n_approved_nodes" v-b-tooltip.hover :title="`${data.item.progress.leaves_n_nodes - data.item.progress.leaves_n_approved_nodes} / ${data.item.progress.leaves_n_nodes} untreated`" /> -->
+            </b-progress>
+          </div>
+          <template
+            slot="action"
+            slot-scope="data"
+          >
+            <b-button
+              size="sm"
+              variant="primary"
+              class="mr-2"
+              :to="{name: 'approve', params: {project_id: data.item.project_id}}"
+            >
+              Approve
+            </b-button>
+            <b-button
+              size="sm"
+              variant="primary"
+              class="mr-2"
+              :to="{name: 'bisect', params: {project_id: data.item.project_id}}"
+            >Bisect</b-button>
+            <b-button
+              size="sm"
+              variant="primary"
+              class="mr-2"
+              @click.prevent="showSaveModal(data.item)"
+            >Save</b-button>
+          </template>
+          <template
+            slot="visible"
+            slot-scope="data"
+          >
+            {{data.visible ? "yes" : "no"}}
+          </template>
+          <template slot="empty">
+            <div class="text-center">No projects available.</div>
+          </template>
+        </b-table>
+        <p v-if="!projects">No projects available.</p>
+        <div style="margin: 0 auto; width: 0;">
+          <b-button
+            size="sm"
+            variant="danger"
+            class="mr-2"
+            href="/labeling"
+          >
+            Expert mode
+          </b-button>
         </div>
-        <b-modal ref="saveModal" lazy centered :title="save_title" @ok="HandleSaveOk">
-            <div class="d-block text-center">
-                <form @submit.stop.prevent="handleSubmit">
-                    <b-form-input type="text" placeholder="Enter your name" v-model="save_slug"></b-form-input>
-                </form>
-            </div>
-        </b-modal>
+      </div>
     </div>
+    <b-modal
+      ref="saveModal"
+      lazy
+      centered
+      :title="save_title"
+      @ok="HandleSaveOk"
+    >
+      <div class="d-block text-center">
+        <form @submit.stop.prevent="HandleSaveOk">
+          <b-container fluid>
+            <b-form-row class="my-1">
+              <b-col sm="3">
+                <label for="form_save_name">Name:</label>
+              </b-col>
+              <b-col sm="9">
+                <b-form-input
+                  type="text"
+                  placeholder="Enter a name for the saved file"
+                  id="form_save_name"
+                  v-model="save_slug"
+                ></b-form-input>
+              </b-col>
+            </b-form-row>
+            <b-row
+              class="my-3"
+              v-if="save_saving"
+            >
+              <b-col sm="12">
+                Saving...
+              </b-col>
+            </b-row>
+          </b-container>
+        </form>
+      </div>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 import * as api from "@/helpers/api.js";
+import Spinner from "vue-infinite-loading/src/components/Spinner";
 
 export default {
     name: "projects",
     props: {},
-    components: {},
+    components: { Spinner },
     data() {
         return {
             fields: [
@@ -74,25 +158,34 @@ export default {
             ],
             projects: [],
             save_slug: "",
-            save_title: ""
+            save_title: "",
+            save_project_id: null,
+            save_saving: false
         };
     },
     methods: {
         showSaveModal(project) {
-            console.log(project);
+            console.log("project", project);
             this.save_slug = project.name;
-            this.save_title = `Save ${project.name}`;
+            this.save_project_id = project.project_id;
+            this.save_title = `Save ${project.name} (${this.save_project_id})`;
+
             this.$refs.saveModal.show();
         },
-        HandleSaveOk() {
-            // TODO
-        },
-        save_project(project_id) {
-            console.log("Saving", project_id, "...");
-            api.saveProject(project_id).then(result => {
-                alert("Project saved: " + result["tree_fn"]);
+        HandleSaveOk(evt) {
+            evt.preventDefault();
+
+            console.log("Saving", this.save_project_id, "...");
+            this.save_saving = true;
+            api.saveProject(this.save_project_id).then(result => {
+                console.log("Project saved: " + result["tree_fn"]);
+                this.save_saving = false;
+                this.$nextTick(() => {
+                    // Wrapped in $nextTick to ensure DOM is rendered before closing
+                    this.$refs.saveModal.hide();
+                });
             });
-        },
+        }
     },
     mounted() {
         // Load node info
