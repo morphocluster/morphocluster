15b3686b934db34a1cc25a46edd1aa2174ea1cad
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2022-03-11 12:05:36 +0000

    Update Docker Container
    - Use environment.default.yml by default
    - Use conda base environment
    - Remove config_docker.py and include docker-defaults in config_default.py

--- docker/morphocluster/run.sh
+++ docker/morphocluster/run.sh
@@ -4,10 +4,9 @@ cp /authorized_keys /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
 . /opt/conda/etc/profile.d/conda.sh
-conda activate morphocluster
+conda activate base
 
 export FLASK_APP=morphocluster
-export MORPHOCLUSTER_SETTINGS=config_docker.py
 
 echo Waiting for Postgres...
 ./wait-for postgres:5432
--- morphocluster/config_default.py
+++ morphocluster/config_default.py
@@ -1,18 +1,18 @@
 # Redis (LRU for caching)
-# REDIS_LRU_URL = ...
+REDIS_LRU_URL = "redis://redis-lru:6379/0"
 
 # Redis for rq
-# RQ_REDIS_URL = ...
+RQ_REDIS_URL = "redis://redis-rq:6379/0"
 
-# Database
+# Database connection
 SQLALCHEMY_DATABASE_URI = (
-    "postgresql://morphocluster:morphocluster@localhost/morphocluster"
+    "postgresql://morphocluster:morphocluster@postgres/morphocluster"
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_OPTIONS = {"connect_args": {"options": "-c statement_timeout=240s"}}
 
 # Project export directory
-PROJECT_EXPORT_DIR = "/tmp"
+PROJECT_EXPORT_DIR = "/data/export"
 
 # Save the results of accept_recommended_objects
 # to enable the calculation of scores like average precision
@@ -21,4 +21,4 @@ SAVE_RECOMMENDATION_STATS = False
 DATASET_PATH = "/data"
 
 # ORDER BY clause for node_get_next_unfilled
-NODE_GET_NEXT_UNFILLED_ORDER_BY = None
+NODE_GET_NEXT_UNFILLED_ORDER_BY = "largest"
