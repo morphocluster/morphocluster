809229eeab0fd54bf1ed137f6fa55edb1d62562f
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2022-03-14 15:00:04 +0000

    Fix api.py
    (Unknown module sklearn.manifold.isomap because of sklearn upgrade.)

--- morphocluster/api.py
+++ morphocluster/api.py
@@ -23,11 +23,8 @@ from flask import request
 from flask.blueprints import Blueprint
 from flask.helpers import url_for
 from flask_restful import reqparse
-from flask_rq2.job import FlaskJob
-from marshmallow import ValidationError
 from redis.exceptions import RedisError
-from rq.queue import Queue
-from sklearn.manifold.isomap import Isomap
+from sklearn.manifold import Isomap
 from timer_cm import Timer
 
 from morphocluster import background, models
@@ -253,7 +250,11 @@ def save_project(project_id):
 
         tree.export_tree(root_id, tree_fn)
 
-        return jsonify({"tree_fn": tree_fn,})
+        return jsonify(
+            {
+                "tree_fn": tree_fn,
+            }
+        )
 
 
 # ===============================================================================
