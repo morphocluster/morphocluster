5f475ecd77c141e5dbfb33e7c86bb45be8c0646a
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2022-03-19 11:58:40 +0000

    feat(Frontend): Dark Mode

--- morphocluster/frontend/src/views/Projects.vue
+++ morphocluster/frontend/src/views/Projects.vue
@@ -1,157 +1,168 @@
 <template>
-  <div id="projects">
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-      <router-link
-        class="navbar-brand text-light"
-        to="/"
-      >MorphoCluster</router-link>
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active text-light">
-          Projects
-        </li>
-      </ul>
-    </nav>
-    <div class="scrollable">
-      <div class="container">
-        <div
-          class="alerts"
-          v-if="alerts.length"
-        >
-          <b-alert
-            :key="a"
-            v-for="a of alerts"
-            dismissible
-            show
-            :variant="a.variant"
-          >
-            {{a.message}}
-          </b-alert>
-        </div>
-        <b-table
-          id="projects_table"
-          striped
-          sort-by="name"
-          :items="projects"
-          :fields="fields"
-          showEmpty
-        >
-          <template slot="table-colgroup">
-            <col class="col-wide" />
-            <col class="col-narrow" />
-          </template>
-          <template v-slot:cell(name)="data">
-            <router-link :to="{name: 'project', params: {project_id: data.item.project_id}}">{{data.item.name}}</router-link>
-          </template>
-           <template v-slot:cell(progress)="data">
-            <b-progress
-              v-if="'progress' in data.item"
-              :max="data.item.progress.leaves_n_nodes"
+    <div id="projects">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark text-light">
+            <router-link class="navbar-brand text-light" to="/"
+                >MorphoCluster</router-link
             >
-              <b-progress-bar
-                variant="success"
-                :value="data.item.progress.leaves_n_filled_nodes"
-                v-b-tooltip.hover
-                :title="`${data.item.progress.leaves_n_filled_nodes} / ${data.item.progress.leaves_n_nodes} filled up`"
-              />
-              <b-progress-bar
-                variant="warning"
-                :value="data.item.progress.leaves_n_approved_nodes - data.item.progress.leaves_n_filled_nodes"
-                v-b-tooltip.hover
-                :title="`${data.item.progress.leaves_n_approved_nodes} / ${data.item.progress.leaves_n_nodes} approved`"
-              />
-            </b-progress>
-          </template>
-          <template v-slot:cell(action)="data">
-            <b-button
-              size="sm"
-              variant="primary"
-              class="mr-2"
-              :to="{name: 'approve', params: {project_id: data.item.project_id}}"
-            >
-              Validate
-            </b-button>
-            <b-button
-              size="sm"
-              variant="primary"
-              class="mr-2"
-              :to="{name: 'bisect', params: {project_id: data.item.project_id}}"
-            >Grow</b-button>
-            <b-button
-              size="sm"
-              variant="primary"
-              class="mr-2"
-              @click.prevent="showSaveModal(data.item)"
-            >Save</b-button>
-          </template>
-          <template v-slot:empty>
-            <div class="text-center">No projects available.</div>
-          </template>
-        </b-table>
-        <p v-if="!projects">No projects available.</p>
-        <div style="margin: 0 auto; width: 0;">
-          <b-button
-            size="sm"
-            variant="danger"
-            class="mr-2"
-            href="/labeling"
-          >
-            Expert mode
-          </b-button>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active text-light">Projects</li>
+            </ul>
+            <dark-mode-control />
+        </nav>
+        <div class="scrollable">
+            <div class="container">
+                <div class="alerts" v-if="alerts.length">
+                    <b-alert
+                        :key="a"
+                        v-for="a of alerts"
+                        dismissible
+                        show
+                        :variant="a.variant"
+                    >
+                        {{ a.message }}
+                    </b-alert>
+                </div>
+                <b-table
+                    id="projects_table"
+                    striped
+                    sort-by="name"
+                    :items="projects"
+                    :fields="fields"
+                    showEmpty
+                >
+                    <template slot="table-colgroup">
+                        <col class="col-wide" />
+                        <col class="col-narrow" />
+                    </template>
+                    <template v-slot:cell(name)="data">
+                        <router-link
+                            :to="{
+                                name: 'project',
+                                params: { project_id: data.item.project_id },
+                            }"
+                            >{{ data.item.name }}</router-link
+                        >
+                    </template>
+                    <template v-slot:cell(progress)="data">
+                        <b-progress
+                            v-if="'progress' in data.item"
+                            :max="data.item.progress.leaves_n_nodes"
+                        >
+                            <b-progress-bar
+                                variant="success"
+                                :value="
+                                    data.item.progress.leaves_n_filled_nodes
+                                "
+                                v-b-tooltip.hover
+                                :title="`${data.item.progress.leaves_n_filled_nodes} / ${data.item.progress.leaves_n_nodes} filled up`"
+                            />
+                            <b-progress-bar
+                                variant="warning"
+                                :value="
+                                    data.item.progress.leaves_n_approved_nodes -
+                                    data.item.progress.leaves_n_filled_nodes
+                                "
+                                v-b-tooltip.hover
+                                :title="`${data.item.progress.leaves_n_approved_nodes} / ${data.item.progress.leaves_n_nodes} approved`"
+                            />
+                        </b-progress>
+                    </template>
+                    <template v-slot:cell(action)="data">
+                        <b-button
+                            size="sm"
+                            variant="primary"
+                            class="mr-2"
+                            :to="{
+                                name: 'approve',
+                                params: { project_id: data.item.project_id },
+                            }"
+                        >
+                            Validate
+                        </b-button>
+                        <b-button
+                            size="sm"
+                            variant="primary"
+                            class="mr-2"
+                            :to="{
+                                name: 'bisect',
+                                params: { project_id: data.item.project_id },
+                            }"
+                            >Grow</b-button
+                        >
+                        <b-button
+                            size="sm"
+                            variant="primary"
+                            class="mr-2"
+                            @click.prevent="showSaveModal(data.item)"
+                            >Save</b-button
+                        >
+                    </template>
+                    <template v-slot:empty>
+                        <div class="text-center">No projects available.</div>
+                    </template>
+                </b-table>
+                <p v-if="!projects">No projects available.</p>
+                <div style="margin: 0 auto; width: 0">
+                    <b-button
+                        size="sm"
+                        variant="danger"
+                        class="mr-2"
+                        href="/labeling"
+                    >
+                        Expert mode
+                    </b-button>
+                </div>
+            </div>
         </div>
-      </div>
+        <b-modal
+            ref="saveModal"
+            lazy
+            centered
+            :title="save_title"
+            @ok="HandleSaveOk"
+        >
+            <div class="d-block text-center">
+                <form @submit.stop.prevent="HandleSaveOk">
+                    <b-container fluid>
+                        <b-form-row class="my-1">
+                            <b-col sm="3">
+                                <label for="form_save_name">Name:</label>
+                            </b-col>
+                            <b-col sm="9">
+                                <b-form-input
+                                    type="text"
+                                    placeholder="Enter a name for the saved file"
+                                    id="form_save_name"
+                                    v-model="save_slug"
+                                ></b-form-input>
+                            </b-col>
+                        </b-form-row>
+                        <b-row class="my-3" v-if="save_saving">
+                            <b-col sm="12"> Saving... </b-col>
+                        </b-row>
+                    </b-container>
+                </form>
+            </div>
+        </b-modal>
     </div>
-    <b-modal
-      ref="saveModal"
-      lazy
-      centered
-      :title="save_title"
-      @ok="HandleSaveOk"
-    >
-      <div class="d-block text-center">
-        <form @submit.stop.prevent="HandleSaveOk">
-          <b-container fluid>
-            <b-form-row class="my-1">
-              <b-col sm="3">
-                <label for="form_save_name">Name:</label>
-              </b-col>
-              <b-col sm="9">
-                <b-form-input
-                  type="text"
-                  placeholder="Enter a name for the saved file"
-                  id="form_save_name"
-                  v-model="save_slug"
-                ></b-form-input>
-              </b-col>
-            </b-form-row>
-            <b-row
-              class="my-3"
-              v-if="save_saving"
-            >
-              <b-col sm="12">
-                Saving...
-              </b-col>
-            </b-row>
-          </b-container>
-        </form>
-      </div>
-    </b-modal>
-  </div>
 </template>
 
 <script>
 import * as api from "@/helpers/api.js";
 
+import DarkModeControl from "@/components/DarkModeControl.vue";
+
 export default {
     name: "projects",
     props: {},
-    components: {},
+    components: { DarkModeControl },
     data() {
         return {
             fields: [
                 //{ key: "project_id", sortable: true },
                 { key: "name", sortable: true },
                 "progress",
-                "action"
+                "action",
             ],
             projects: [],
             save_slug: "",
@@ -175,7 +186,7 @@ export default {
 
             console.log("Saving", this.save_project_id, "...");
             this.save_saving = true;
-            api.saveProject(this.save_project_id).then(result => {
+            api.saveProject(this.save_project_id).then((result) => {
                 console.log("Project saved: " + result["tree_fn"]);
                 this.save_saving = false;
                 this.$nextTick(() => {
@@ -183,31 +194,33 @@ export default {
                     this.$refs.saveModal.hide();
                 });
             });
-        }
+        },
     },
     mounted() {
         // Load node info
         api.getProjects()
-            .then(projects => {
+            .then((projects) => {
                 this.projects = projects;
 
                 return Promise.all(
-                    this.projects.map(p => {
-                        return api.getNodeProgress(p.node_id).then(progress => {
-                            console.log(`Got progress for ${p.node_id}.`);
-                            this.$set(p, "progress", progress);
-                        });
+                    this.projects.map((p) => {
+                        return api
+                            .getNodeProgress(p.node_id)
+                            .then((progress) => {
+                                console.log(`Got progress for ${p.node_id}.`);
+                                this.$set(p, "progress", progress);
+                            });
                     })
                 );
             })
-            .catch(e => {
+            .catch((e) => {
                 console.log(e);
                 this.alerts.unshift({
                     message: e.message,
-                    variant: "danger"
+                    variant: "danger",
                 });
             });
-    }
+    },
 };
 </script>
 
