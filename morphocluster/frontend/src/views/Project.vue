3dd3a9add1a8fe6d128f5e7a9e4a1ec08c4f0d2d
Torge Schwark <torge.schwark@outlook.de>
2023-06-19 16:11:53 +0200

    created the project page

--- morphocluster/frontend/src/views/Project.vue
+++ morphocluster/frontend/src/views/Project.vue
@@ -1,17 +1,48 @@
 <template>
-    <div class="container">
-         {{project}}
+    <div id="Project">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark text-light">
+            <router-link class="navbar-brand text-light" to="/"
+                >MorphoCluster</router-link
+            >
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active text-light">Projects</li>
+            </ul>
+            <dark-mode-control />
+        </nav>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th> Created on </th>
+                    <th> Name </th>
+                    <th> Node_id </th>
+                    <th> Project_id </th>
+                    <th> Visible </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> {{ project.name }}</td> <!-- hier muss noch das datum created on hin -->
+                    <td> {{ project.name }}</td>
+                    <td> {{ project.node_id }}</td>
+                    <td> {{ project.project_id }}</td>
+                    <td> {{ project.name }}</td> <!-- hier muss noch der visible wert hin -->
+                </tr>
+            </tbody>
+        </table> 
     </div>
 </template>
 
 <script>
 import axios from "axios";
 import { EventBus } from "@/event-bus.js";
+import DarkModeControl from "@/components/DarkModeControl.vue";
+
 
 export default {
     name: "ProjectView",
     props: {"project_id": Number},
-    components: {},
+    components: {DarkModeControl},
     data() {
         return {
             project: null,
@@ -20,7 +51,7 @@ export default {
     methods: {
 
     },
-    mounted() {
+    mounted() {4
         // Load node info
         axios
             .get(`/api/projects/${this.project_id}`)
@@ -38,5 +69,24 @@ export default {
 </script>
 
 <style>
+#Project {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    flex: 1;
+    overflow: hidden;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
 
+th, td {
+  padding: 8px;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  background-color: #f2f2f2;
+}
 </style>
