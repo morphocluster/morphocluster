bfa487c7ab9d8a0ecbee3c72e421a540c082f87f
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2024-01-25 20:01:14 +0000

    Undoing what shouldn't have been done

--- morphocluster/frontend/src/globalState.js
+++ morphocluster/frontend/src/globalState.js
@@ -1,45 +1,20 @@
-// globalState.js
-
 import Vue from 'vue';
 
-const state = new Vue({
+export default new Vue({
     data: () => {
         return { breadcrumbs: [], loading: [] }
     },
     methods: {
-        setBreadcrumbs(breadcrumbs, type) {
+        /**
+         * Set the breadcrumbs in the app bar.
+         * @param breadcrumbs - A list of breadcrumb items
+         * @see https://v2.vuetifyjs.com/en/components/breadcrumbs/
+         */
+        setBreadcrumbs(breadcrumbs) {
             console.log("setBreadcrumbs", breadcrumbs);
-            let current_path = ""
-            let itt = 0
-            let toPath = ""
-            const breadcrumbItems = breadcrumbs.map(breadcrumbText => {
-                if (type === "files") {
-                    if (itt === 0) {
-                        toPath = { text: breadcrumbText, disabled: false, to: "/files/" };
-                    } else if (itt === 1) {
-                        toPath = { name: 'files', params: { file_path: breadcrumbText } }
-                        current_path += breadcrumbText
-                    } else {
-                        current_path += "/" + breadcrumbText
-                        toPath = { name: 'files', params: { file_path: current_path } }
-                    }
-                    itt += 1;
-                } else if (type === "project" && itt === 1) {
-                    toPath = { name: 'project', params: { project_id: breadcrumbText }, }
-                    breadcrumbText = str(breadcrumbText)
-                }
-                else if (type === "projects" || (type === " project" && itt === 0)) {
-                    toPath = { name: 'projects' }
-                    itt += 1
-                }
-                return { text: breadcrumbText, disabled: false, to: toPath };
-            });
-            if (type == "home") {
-                this.breadcrumbs = [];
-            }
-            this.breadcrumbs = breadcrumbItems;
+            this.breadcrumbs = breadcrumbs;
 
-            const titles = breadcrumbItems.map(b => b.text);
+            const titles = breadcrumbs.map(b => b.text);
 
             document.title = "MorphoCluster - " + titles.join(" / ");
         },
@@ -56,7 +31,4 @@ const state = new Vue({
             this.loading = this.loading.filter(item => item !== name);
         }
     }
-});
-
-export default state;
-export { state };
+})
