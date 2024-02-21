c08c26169ae84acb848601360342dbb3864c0f0b
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2024-02-21 14:44:47 +0000

    MemberCollection.support_vector: Support and cumulative vector
    This moves the responsibility to calculate support and cumulative vector from the Tree to the MemberCollection and fixes issues with None vectors (which where previously converted to zero vectors).

--- morphocluster/member.py
+++ morphocluster/member.py
@@ -6,18 +6,17 @@ Created on 10.08.2018
 
 import numpy as np
 from collections.abc import Sequence
-from morphocluster.helpers import seq2array
 
 
 class MemberCollection(Sequence):
     """
     A collection of nodes or objects or both.
-    
-    Nodes have a _centroid key, objects have a _ 
-    
+
+    Nodes have a _centroid key, objects have a _
+
     Parameters:
         members: list of dict
-        none_action: "raise" | "zero" | "remove"
+        none_action: "raise" | "remove"
             How to deal with None vector values.
     """
 
@@ -38,10 +37,10 @@ class MemberCollection(Sequence):
             if self.none_action == "raise":
                 n_none = sum(1 for v in vectors if v is None)
                 if n_none:
-                    raise ValueError(f"vectors contain {n_none} None entries (out of {len(vectors)})")
+                    raise ValueError(
+                        f"vectors contain {n_none} None entries (out of {len(vectors)})"
+                    )
                 self._vectors = np.array(vectors)
-            elif self.none_action == "zero":
-                self._vectors = seq2array(vectors, len(vectors))
             elif self.none_action == "remove":
                 self._vectors = np.array([v for v in vectors if v is not None])
             else:
@@ -49,19 +48,28 @@ class MemberCollection(Sequence):
 
             return self._vectors
 
-    @property
-    def cardinalities(self):
-        try:
-            return self._cardinalities
-        except AttributeError:
-            cardinalities = [
-                m["_n_objects_deep"] if "_n_objects_deep" in m else 1
-                for m in self.members
-            ]
+    def get_support_and_vector(self):
+        """
+        Calculate the support of this collection and its cumulative vector.
 
-            self._cardinalities = np.array(cardinalities)
+        This is used in the recursive calculation of cluster centroids.        
+        """
+        support = 0
+        vector = 0
 
-            return self._cardinalities
+        for m in self.members:
+            vector = m["_centroid"] if "_centroid" in m else m["vector"]
+            if vector is None:
+                continue
+
+            cardinality = m["_n_objects_deep"] if "_n_objects_deep" in m else 1
+            if cardinality == 0:
+                continue
+
+            vector = vector + cardinality * vector
+            support += cardinality
+
+        return support, vector
 
     # Abstract methods
     def __getitem__(self, index):
