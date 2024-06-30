f7828ef0ea0e7447f6815351b35d4185bfca1c98
Martin Schroeder <mschroeder@geomar.de>
2024-06-30 16:19:58 +0000

    Fix packaging

--- lib/setup.py
+++ lib/setup.py
@@ -1,11 +1,11 @@
 import versioneer
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 setup(
     name="morphocluster.lib",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages("src/", include=["morphocluster.lib"]),
+    packages=find_namespace_packages("src/", include=["morphocluster.lib"]),
     package_dir={"": "src"},
     install_requires=["chardet", "click", "numpy", "scikit-learn", "h5py", "hdbscan"],
     entry_points={
--- server/setup.py
+++ server/setup.py
@@ -1,10 +1,9 @@
-from setuptools import find_packages, setup
-
 import versioneer
+from setuptools import find_namespace_packages, setup
 
 setup(
     name="morphocluster.server",
-    packages=find_packages("src/", include=["morphocluster.server"]),
+    packages=find_namespace_packages("src/", include=["morphocluster.server"]),
     package_dir={"": "src"},
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
